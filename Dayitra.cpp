e9bd673f1b1f2bf2eaedb6cbc0a35cac47be8079
JengoWins <unreal.gold@mail.ru>
2023-09-12 17:43:19 +0300

    Create WhileDestroy

---
+++ Dayitra.cpp
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include "Algoritm.h"
+
+using namespace std;
+
+int main()
+{
+	int force= 0
+	while (true)
+	{
+		force++;
+	}
+}
